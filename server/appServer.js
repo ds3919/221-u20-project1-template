ca2d8b48cf5a6094bb2f4d711c99876fdd819a6f
ds3919 <79234944+ds3919@users.noreply.github.com>
2022-07-05 11:58:52 -0400

    Added feed URL path and added 2 divs

--- server/appServer.js
+++ server/appServer.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express();
 app.use(express.static('client/public'));
 
+
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: './client/views'})
 })
 
+app.get('/feed', function(req, res) {
+    res.sendFile('feed.html', {root: './client/views'})
+})
 
 
 app.listen(1337, () => console.log('Listening on port 1337.'))
