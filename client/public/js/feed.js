ea27b329c115e6acc9c5449a3587fac61e5c356f
ds3919 <79234944+ds3919@users.noreply.github.com>
2022-07-13 15:35:24 -0400

    WIP delete button

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -3,10 +3,14 @@ window.addEventListener('load', () => {
         res.json().then((data) => {
             for(var i = 0; i < data.length; i++) {
                 let item = data[i];
-                document.getElementById('newsfeed').innerHTML += "<img class='feedImage' src='" + item.imageUrl+ "'>\n<p><a href='" + item.linkUrl + "'>" + item.title + "</a><br>" + item.body + "<br><hr></p><br>";
+                document.getElementById('newsfeed').innerHTML += "<img class='feedImage' src='" + item.imageUrl+ "'>\n<p><a href='" + item.linkUrl + "'>" + item.title + "</a><br>" + item.body + "<br><button class='delete' id='" + i + "'>Delete</button><br><hr></p><br>";
             }
         });
     });
 });
 
 document.getElementById('portal_button').addEventListener('click', () => {location='http://www.marist.edu'})
+
+document.getElementsByClassName('delete').addEventListener('click', function() {
+    fetch('/api/feeditems/'+event.srcElement.id, {method:'DELETE'})
+});
